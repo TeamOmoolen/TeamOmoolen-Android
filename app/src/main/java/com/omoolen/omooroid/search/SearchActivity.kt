886074e2f17ed44db445742bf13ee6f0e1fa548f
You-jeong136 <smj06168@gmail.com>
2021-07-16 08:59:05 +0900

    [feat] SearchResultActivity 추가 ( 서버 O )

--- app/src/main/java/com/omoolen/omooroid/search/SearchActivity.kt
+++ app/src/main/java/com/omoolen/omooroid/search/SearchActivity.kt
@@ -1,7 +1,11 @@
 package com.omoolen.omooroid.search
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
@@ -9,9 +13,12 @@ import com.google.android.material.tabs.TabLayout
 import com.omoolen.omooroid.R
 import com.omoolen.omooroid.databinding.ActivitySearchBinding
 import com.omoolen.omooroid.search.fragment.SearchFragmentAdapter
+import com.omoolen.omooroid.search.search_result.SearchResultActivity
 
 
 class SearchActivity : AppCompatActivity() {
+    var imm : InputMethodManager? = null
+
     private var _binding: ActivitySearchBinding? = null
     private val binding get() = _binding ?: error("View를 참조하기 위해 binding이 초기화되지 않았습니다.")
 
@@ -23,7 +30,20 @@ class SearchActivity : AppCompatActivity() {
         setContentView(binding.root)
         setPageAdapter()
 
+        var isKeyboard : String? = intent?.getStringExtra("keyboard")
+        if(isKeyboard != null)
+            binding.etSearch.requestFocus()
+
         clickEvents()
+
+        imm = getSystemService(android.content.Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+
+    }
+
+    fun hideKeyboard(v: View){
+        if(v != null){
+            imm?.hideSoftInputFromWindow(v.windowToken, 0)
+        }
     }
 
     private fun clickEvents() {
@@ -35,7 +55,10 @@ class SearchActivity : AppCompatActivity() {
         //검색 이벤트 동작
         binding.clSearchTouch.setOnClickListener {
             //et_search에 들어온 값 서버에 get 요청하기
-            viewModel.getSearch(binding.etSearch.text.toString())
+            //viewModel.getSearch(binding.etSearch.text.toString())
+            val intent = Intent(this, SearchResultActivity::class.java)
+            intent.putExtra("keyword", binding.etSearch.text.toString())
+            startActivity(intent)
 
             Log.d("SEARCH_ACTIVITY", "검색클릭")
             viewModel.addRecent(binding.etSearch.text.toString())
