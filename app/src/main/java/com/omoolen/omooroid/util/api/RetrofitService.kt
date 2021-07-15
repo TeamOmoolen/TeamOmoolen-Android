0acd1571db5b564fd53d7aa814c83109b32127b6
jiwon_ryu <happyjw3910@gmail.com>
2021-07-15 15:54:31 +0900

    [FEAT] 키워드 검색 조회 성공

--- app/src/main/java/com/omoolen/omooroid/util/api/RetrofitService.kt
+++ app/src/main/java/com/omoolen/omooroid/util/api/RetrofitService.kt
@@ -1,11 +1,15 @@
 package com.omoolen.omooroid.util.api
 
 import com.omoolen.omooroid.search.data.Data
+import com.omoolen.omooroid.search.data.KeywordSearch
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface RetrofitService {
     //키워드 검색
     @GET("api/searchProduct")
-    fun getData() : Single<Data>
+    fun getData(
+        @Query("keyword") keyword : String
+    ) : Single<KeywordSearch>
 }
